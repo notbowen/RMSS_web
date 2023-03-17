cd1b21da6986057b09c6b86cdf6b687a64856890
wHo69 <hu_bowen@students.edu.sg>
2023-03-17 20:53:50 +0800

    MCQ export bugfixes

--- app/questions_backend/word.py
+++ app/questions_backend/word.py
@@ -28,7 +28,7 @@ class HTMLHelper(object):
                     run_map.append((text, "plain_text"))
             run_map.append((match.group(2), match.group(1)))
             ptr = match.end()
-        if ptr < len(html_fragment) - 1:
+        if ptr <= len(html_fragment) - 1:
             run_map.append((html_fragment[ptr:], "plain_text"))
         return run_map
 
@@ -86,6 +86,7 @@ def parse_mcq_question(questions: List[Question], doc: docx.Document) -> None:
     prev_root = doc.add_paragraph()
     prev_root.paragraph_format.left_indent = docx.shared.Cm(0.5)
     prev_root.style = doc.styles["Question"]
+    prev_root.paragraph_format.keep_with_next = True
 
     # Loop through and add questions
     for i, question in enumerate(questions):
@@ -95,20 +96,42 @@ def parse_mcq_question(questions: List[Question], doc: docx.Document) -> None:
         else:
             root = doc.add_paragraph()
             root.paragraph_format.left_indent = docx.shared.Cm(0.5)
+            root.paragraph_format.space_before = docx.shared.Pt(20)
             root.style = doc.styles["Question"]
+            root.paragraph_format.keep_with_next = True
 
         # Add question
         for j, block in enumerate(question.content["blocks"]):
+            # Create new paragraph if not first block
+            if j != 0:
+                para = doc.add_paragraph()
+                para.paragraph_format.left_indent = docx.shared.Cm(0.5)
+                para.paragraph_format.space_before = docx.shared.Pt(8)
+                para.style = doc.styles["Question"]
+                para.paragraph_format.keep_with_next = True
+            else:
+                para = root
+
+            # Override space_after to 0 if last block, for answer place
+            if j == len(question.content["blocks"]) - 1:
+                para.paragraph_format.space_after = docx.shared.Pt(0)
+
             if block["type"] == "paragraph":
-                if j != len(question.content["blocks"]) - 1:
-                    is_next_table = question.content["blocks"][j + 1]["type"] == "table"
-                    add_text(block["data"]["text"], root, is_last=False, is_next_table=is_next_table)
-                else:
-                    add_text(block["data"]["text"], root, is_last=True)
+                add_text(block, para)
             elif block["type"] == "image":
-                add_image(block, root)
+                add_image(block, para)
             elif block["type"] == "table":
                 add_table(block, doc)
+
+        # Add paragraph for answer place
+        ans_para = doc.add_paragraph()
+        ans_para.paragraph_format.left_indent = docx.shared.Cm(0)
+        ans_para.paragraph_format.space_before = docx.shared.Pt(0)
+        ans_para.style = doc.styles["Question"]
+
+        # Add text for answer place
+        ans_para.add_run("(        )")
+        ans_para.paragraph_format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.RIGHT
 
         # Make paragraph a list to have numbering
         if i != 0:
@@ -159,6 +182,9 @@ def add_mcq_answers(questions: List[Question], doc: docx.Document) -> None:
     # Create table
     answer_table = doc.add_table(rows=0, cols=4)
     answer_table.style = "Table Grid"
+
+    # Align table to centre
+    answer_table.alignment = docx.enum.table.WD_TABLE_ALIGNMENT.CENTER
     
     # Calculate total rows to be in table
     num_questions = len(questions)
@@ -209,7 +235,25 @@ def add_structured_answers(questions: List[Question], doc: docx.Document) -> Non
 
     pass
 
-def add_text(text: str, paragraph: docx.text.paragraph.Paragraph, is_last=False, is_table=False, is_next_table=False) -> None:
+def add_text(block: Dict[str, str], paragraph: docx.text.paragraph.Paragraph) -> None:
+    """Add text to a paragraph
+
+    Args:
+        block (Dict[str, str]): EditorJS block to add
+        paragraph (docx.text.paragraph.Paragraph): Paragraph to add to
+    """
+
+    # Get alignment, default to left
+    try:
+        alignment = block["data"]["alignment"]
+    except KeyError:
+        alignment = "left"
+
+    # Add text to paragraph
+    add_text_from_str(block["data"]["text"], paragraph, alignment=alignment)
+    
+
+def add_text_from_str(text: str, paragraph: docx.text.paragraph.Paragraph, alignment: str = "left") -> None:
     """Add text to a paragraph
 
     Args:
@@ -227,13 +271,13 @@ def add_text(text: str, paragraph: docx.text.paragraph.Paragraph, is_last=False,
     # Add text to paragraph and add new line if not a table
     html_helper.insert_runs_from_html_map(paragraph, run_map)
 
-    if is_table or is_next_table:
-        return
-
-    if not is_last:
-        paragraph.add_run("\n\n")
-    else:
-        paragraph.add_run("\n")
+    # Set alignment
+    if alignment == "left":
+        paragraph.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.LEFT
+    elif alignment == "right":
+        paragraph.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.RIGHT
+    elif alignment == "center":
+        paragraph.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
 
 def add_image(block: Dict[str, str], paragraph: docx.text.paragraph.Paragraph) -> None:
     """Add image to a paragraph
@@ -251,19 +295,18 @@ def add_image(block: Dict[str, str], paragraph: docx.text.paragraph.Paragraph) -
     rel_path = "./app" + image_path
 
     # Add image
-    image = paragraph.add_run("    ")
+    image = paragraph.add_run()
     image.add_picture(rel_path, width=docx.shared.Cm(13))
 
-    # Add new line
-    paragraph.add_run("\n\n")
+    # Align image to centre
+    paragraph.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
 
 def add_table(block, doc) -> None:
-    # Check if withHeadings is true
-    # TODO: Check if withHeadings is a boolean or string
-    try:
-        with_headings = block["data"]["withHeadings"]
-    except KeyError:
-        with_headings = False
+    # Remove last paragraph to prevent newline above table
+    delete_paragraph(doc.paragraphs[-1])
+
+    # Get the value of withHeadings
+    with_headings = block["data"]["withHeadings"]
 
     # Get table data
     table_data = block["data"]["content"]
@@ -272,6 +315,9 @@ def add_table(block, doc) -> None:
     table = doc.add_table(rows=len(table_data), cols=len(table_data[0]))
     table.style = "Table Grid"
 
+    # Force table to be on one page
+    table.allow_autofit = False
+
     # Add indent to table
     tbl_pr = table._tblPr
     e = OxmlElement("w:tblInd")
@@ -279,24 +325,31 @@ def add_table(block, doc) -> None:
     e.set(qn("w:type"), "dxa")
     tbl_pr.append(e)
 
+    # Set table height to 0.74cm
+    for row in table.rows:
+        row.height = docx.shared.Cm(0.74)
+
     # Add table data
     for i, row in enumerate(table_data):
         for j, cell in enumerate(row):
+            # Get current cell
+            curr_cell = table.cell(i, j)
+
+            # Align cell to centre
+            curr_cell.vertical_alignment = docx.enum.table.WD_CELL_VERTICAL_ALIGNMENT.CENTER
+
             # Set paragraph style
-            p = table.cell(i, j).paragraphs[0]
+            p = curr_cell.paragraphs[0]
             p.style = doc.styles["Question"]
             p.paragraph_format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
 
             # Add text to cell
-            add_text(cell, p, is_table=True)
+            add_text_from_str(cell, p, alignment="center")
 
     # Shade first row if withHeadings is true
     if with_headings:
         for cell in table.rows[0].cells:
             set_table_header_bg_color(cell)
-
-    # Add new line
-    doc.add_paragraph("\n")
 
 def process_text(text: str) -> str:
     text = text.replace("&nbsp;", " ")
@@ -359,6 +412,16 @@ def set_table_header_bg_color(cell):
     tblCell = cell._tc
     tblCellProperties = tblCell.get_or_add_tcPr()
     clShading = OxmlElement('w:shd')
-    clShading.set(qn('w:fill'), "707070")
+    clShading.set(qn('w:fill'), "D9D9D9")
     tblCellProperties.append(clShading)
     return cell
+
+def delete_paragraph(paragraph):
+    """Delete a paragraph from the document.
+
+    Args:
+        paragraph (docx.text.paragraph.Paragraph): Paragraph to delete
+    """
+    p = paragraph._element
+    p.getparent().remove(p)
+    paragraph._p = paragraph._element = None
