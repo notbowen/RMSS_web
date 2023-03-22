8e5d81c35f57033a4ddfa145241a9e3d0258b18d
wHo69 <hu_bowen@students.edu.sg>
2023-03-22 20:37:44 +0800

    Added edit topic functionality

---
+++ app/static/js/edit_topics.js
@@ -0,0 +1,115 @@
+// Detect enter key press function
+$.fn.enterKey = function (fnc) {
+    return this.each(function () {
+        $(this).keypress(function (ev) {
+            var keycode = (ev.keyCode ? ev.keyCode : ev.which);
+            if (keycode == '13') {
+                fnc.call(this, ev);
+            }
+        })
+    })
+}
+
+// Main
+$(document).ready(function($) {
+    // Hide the save and cancel buttons
+    $(document).find(".btn_save").hide();
+    $(document).find(".btn_cancel").hide();
+
+    // Make whole table row editable
+    $(document).on("click", ".btn_edit", function(event) {
+        event.preventDefault();
+        var tbl_row = $(this).closest("tr");
+
+        tbl_row.find(".btn_save").show();
+        tbl_row.find(".btn_cancel").show();
+
+        tbl_row.find(".btn_edit").hide();
+
+        // Make whole row editable
+        tbl_row.find(".row_data")
+            .attr("contenteditable", "true")
+            .attr("edit_type", "button")
+            .addClass("bg-warning")
+            .css("padding", "3px");
+
+        // Backup original entry
+        tbl_row.find(".row_data").each(function(index, val) {
+            $(this).attr("original_entry", $(this).html());
+        });
+    });
+
+    // On user click cancel
+    $(document).on("click", ".btn_cancel", function(event) {
+        event.preventDefault();
+        var tbl_row = $(this).closest("tr");
+
+        // Hide save & cancel buttons
+        tbl_row.find(".btn_save").hide();
+        tbl_row.find(".btn_cancel").hide();
+
+        // Show edit button
+        tbl_row.find(".btn_edit").show();
+
+        // Make the whole row uneditable
+        tbl_row.find(".row_data")
+            .attr("contenteditable", "false")
+            .attr("edit_type", "click")
+            .removeClass("bg-warning")
+            .css("padding", "");
+
+        // Restore original data
+        tbl_row.find(".row_data").each(function(index, val) {
+            $(this).html($(this).attr("original_entry"));
+        });
+    });
+
+    // Save whole table row entry
+    $(document).on("click", ".btn_save", function(event) {
+        event.preventDefault();
+
+        var tbl_row = $(this).closest("tr");
+        var row_id = tbl_row.attr("row_id");
+
+        // Hide save & cancel buttons
+        tbl_row.find(".btn_save").hide();
+        tbl_row.find(".btn_cancel").hide();
+
+        // Show edit button
+        tbl_row.find(".btn_edit").show();
+
+        // Make the whole row uneditable
+        tbl_row.find(".row_data")
+            .attr("contenteditable", "false")
+            .attr("edit_type", "click")
+            .removeClass("bg-warning")
+            .css("padding", "");
+
+        // Get row data
+        var arr = {};
+        tbl_row.find(".row_data").each(function(index, val) {
+            var col_name = $(this).attr("col_name");
+            var col_val = $(this).text();
+
+            arr[col_name] = col_val;
+        });
+
+        // Send to server
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "/topics/edit?id=" + row_id, true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status !== 200) {
+                alert("Failed to edit topic!\nError: " + xhr.responseText);
+            }
+        }
+
+        xhr.send(JSON.stringify(arr));
+    });
+
+    // Lose focus when user presses enter
+    $(".row_data").enterKey(function() {
+        $(".row_data").blur();
+    });
+});
--- app/topics/routes.py
+++ app/topics/routes.py
@@ -1,9 +1,45 @@
-from flask import render_template
+from flask import render_template, request
 
 from app.topics import bp
 from app.models.category import Category
+from app.extensions import db
 
-@bp.route("/edit")
+@bp.route("/edit", methods=["GET", "POST"])
 def edit_topics():
+    # Handle POST request
+    if request.method == "POST":
+        # Get ID from URL query parameters
+        id = request.args.get("id", type=int)
+
+        # Ensure ID is present
+        if id == None:
+            return "Missing ID", 400
+
+        # Get data
+        content = request.get_json()
+
+        # Ensure data is valid
+        if not content:
+            return "No data provided", 400
+        
+        # Ensure ID is valid
+        category = Category.query.get(id)
+        if not category:
+            return "Invalid ID", 400
+
+        # Update category
+        try:
+            category.level = content["level"]
+            category.subject = content["subject"]
+            category.topic = content["topic"]
+        except KeyError:
+            return "Missing data", 400
+
+        # Save changes
+        db.session.commit()
+
+        return "OK", 200
+
+    # Handle GET request
     categories = Category.query.all()
     return render_template("edit_topics.html", categories=categories)
