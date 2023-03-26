82b9c1cc220ab38eddde76257e894ca139a7e8a8
wHo69 <hu_bowen@students.edu.sg>
2023-03-26 20:15:02 +0800

    Added user warnings & delete template client side

--- app/static/js/question_templates.js
+++ app/static/js/question_templates.js
@@ -5,6 +5,13 @@ function templateCardClicked(templateId) {
 
 // When remove question button is clicked, remove the question from the template
 function removeQuestionClicked(questionId, templateId) {
+    // Ask user for confirmation
+    var confirmation = confirm("Are you sure you want to remove " + questionId + " from the template?");
+    if (!confirmation) {
+        return;
+    }
+    
+    // Send request to remove question from template
     var xhr = new XMLHttpRequest();
     xhr.open("DELETE", "/templates/" + templateId + "/delete_question?id=" + questionId, true);
 
@@ -17,6 +24,33 @@ function removeQuestionClicked(questionId, templateId) {
             alert("Something went wrong!\nError: " + xhr.responseText);
         } else {
             window.location.reload();
+        }
+    };
+
+    xhr.send();
+}
+
+// When delete template button is clicked, delete the template
+function deleteTemplateClicked(templateId) {
+    // Ask user for confirmation
+    var confirmation = confirm("Are you sure you want to delete this template?");
+    if (!confirmation) {
+        return;
+    }
+
+    // Send request to delete template
+    var xhr = new XMLHttpRequest();
+    xhr.open("DELETE", "/templates/delete?id=" + templateId, true);
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState !== 4) {
+            return;
+        }
+
+        if (xhr.status !== 200) {
+            alert("Something went wrong!\nError: " + xhr.responseText);
+        } else {
+            window.location.href = "/templates";
         }
     };
 
